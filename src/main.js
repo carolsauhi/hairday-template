c55a59f2ab3d8f3cc7cfba4f8a760adcfb0bcc21
carolsauhi <carol_sauhi@hotmail.com>
2025-03-24 20:55:45 -0300

    Definindo a data atual

--- src/main.js
+++ src/main.js
@@ -8,5 +8,5 @@ import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
 
-// import dayjs from "dayjs"
-// console.log(dayjs().format("DD/MM - HH:mm"))
+// JS
+import "./modules/form/submit.js"
