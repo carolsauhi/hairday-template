4a9a5fbf196e6ab23789d42a8e15364bf39c1f21
carolsauhi <carol_sauhi@hotmail.com>
2025-03-25 10:24:10 -0300

    Criando o serviço de agendamento

---
+++ src/services/schedule-new.js
@@ -0,0 +1,18 @@
+import {apiConfig} from "./api-config.js"
+
+export async function scheduleNew({ id, name, when }){
+  try {
+    await fetch(`${apiConfig.baseURL}/schedule`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({id, name, when}),
+    })
+
+    alert("Agendamento realizado!")
+  } catch (error) {
+    alert("Não foi possível agendar. Tente novamente mais tarde.")
+    console.log(error)
+  }
+}
