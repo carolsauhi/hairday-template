e3892bec442c2021c3e816b91c03b1ef06af8d95
carolsauhi <carol_sauhi@hotmail.com>
2025-03-24 22:17:46 -0300

    Selecionando um horário

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,6 @@
 import { openingHours } from "../../utils/opening-hours.js"
 import dayjs from "dayjs"
+import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
@@ -35,6 +36,9 @@ export function hoursLoad({ date }){
       hourHeaderAdd("Noite")
     hours.append(li)
   })
+
+  //Adiciona o evento de clique nos horários disponíveis
+  hoursClick()
 }
 
 function hourHeaderAdd(title){
