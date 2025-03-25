e2c89bd909cd85b7132545d97b038609178cf545
carolsauhi <carol_sauhi@hotmail.com>
2025-03-24 22:35:31 -0300

    Carregando horários para outros dias

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,15 +1,17 @@
-import { openingHours } from "../../utils/opening-hours.js"
 import dayjs from "dayjs"
+
+import { openingHours } from "../../utils/opening-hours.js"
 import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
 export function hoursLoad({ date }){
+  // Limpa a lista de horários
+  hours.innerHTML = ""
   const opening = openingHours.map((hour)=> {
     // Recupera somente a hora
     const [schedulesHour] = hour.split(":")
     
-
     // Adiciona a hora na data e verifica se está no passado
     const isHourPast = dayjs(date).add(schedulesHour, "hour").isAfter(dayjs())
 
